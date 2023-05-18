0a85b20982fb30ad66f43d0e7a91acc5fe81d438
MostofaAsik <mmostofaasik@gmail.com>
2023-05-18 22:25:49 +0600

    banner done

--- src/pages/Home/Home/Banner.jsx
+++ src/pages/Home/Home/Banner.jsx
@@ -1,15 +1,23 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-unused-vars */
 import React from 'react';
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen" style={{ backgroundImage: `url("/images/stock/photo-1507358522600-9f71e620c44e.jpg")` }}>
+        <div className="hero min-h-screen" style={{ backgroundImage: `url("https://images.unsplash.com/photo-1598084991519-c90900bc9df0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dG95JTIwY2FyfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60")` }}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <h1 className="mb-5 text-5xl text-lime-200 font-bold">MAS TOYS CENTER</h1>
+                    <p className="mb-5">
+                        What you are want ,it's here.
+
+                    </p>
+                    <p className="mb-5">
+                        If you are Happay,we will be happy.
+
+                    </p>
+                    <button className="btn btn-primary">Explore More</button>
                 </div>
             </div>
         </div>
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home page</h2>
-        </div>
+        <>
+            <Banner></Banner>
+        </>
     );
 };
 
